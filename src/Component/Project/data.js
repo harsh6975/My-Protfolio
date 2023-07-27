689010be49034c9fe85c7a273d749b5523aa9eb3
harsh6975 <harshu1234ashv@gmail.com>
2023-07-28 01:12:33 +0530

    project phase 1

---
+++ src/Component/Project/data.js
@@ -0,0 +1,46 @@
+import p11 from "../../Images/Project/1/1.png";
+import p12 from "../../Images/Project/1/2.png";
+import p13 from "../../Images/Project/1/3.png";
+import p14 from "../../Images/Project/1/4.png";
+import p15 from "../../Images/Project/1/5.png";
+import p1 from "../../Images/Project/1/1.png";
+
+export const projects = [
+  {
+    name: "CodeBook",
+    banner: p11,
+    tools: ["React", "EJS", "MongoDB"],
+    img: [p11, p12, p13, p14, p15],
+    link: "",
+  },
+  {
+    name: "project1",
+    banner: p1,
+    tools: [],
+    img: [],
+  },
+  {
+    name: "project1",
+    banner: p1,
+    tools: [],
+    img: [],
+  },
+  {
+    name: "project1",
+    banner: p1,
+    tools: [],
+    img: [],
+  },
+  {
+    name: "project1",
+    banner: p1,
+    tools: [],
+    img: [],
+  },
+  {
+    name: "project1",
+    banner: p1,
+    tools: [],
+    img: [],
+  },
+];
